c8c2316d20bcf06997c2361584cbbca791e39675
pradeepfh@123 <pradeepfhadi@gmail.coom>
2022-06-10 16:33:40 +0530

    Account sheet added

--- src/main/java/com/qa/opencart/constants/Constants.java
+++ src/main/java/com/qa/opencart/constants/Constants.java
@@ -41,6 +41,8 @@ public class Constants {
 	public static final String REGISTER_SHEET_NAME = "Register";
 
 	public static final String PRODUCT_SHEET_NAME = "Product";
+	
+	public static final String ACCOUNT_SHEET_NAME = "Account";
 
 	
 
